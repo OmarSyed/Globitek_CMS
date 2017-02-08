88d91b23acb582d563a0bb97e7fe7fb154821b93
OmarSyed <omarsyed26@gmail.com>
2017-02-08 01:27:32 -0500

    Update register.php

--- register.php
+++ register.php
@@ -1,16 +1,25 @@
-
-
-
-<!DOCTYPE html>
-<html> 
-<body>
-<?php $page_title = 'Register'; ?>
-<?php include(SHARED_PATH.'/header.php'); ?>
-
 <?php
   require_once('..\private\initialize.php');
-
+  include(SHARED_PATH.'/header.php'); 
+  ?>
+  <?php $page_title = 'Register'; ?>
+  <div id="main-content">
+  <h1>Register</h1>
+  <p>Register to become a Globitek Partner.</p>
   
+  <form action = "register.php" method ="post">
+    First name: <input type="text" name = "first_name" value=""/>
+    Last name: <input type="text" name="last_name" value=""/><br/>
+    Email: <input type="text" name="email" value =""/><br/>
+    Username:<input type="text" name="username" value=""/><br />
+    <br />
+    <input type="submit" name="submit" value="Submit" />
+	
+  
+  </form>
+</div>
+<?php
+if (isset($_POST['submit'])){ 
 // Set default values for all variables the page needs.
   $firstname = '';
   $lastname = '';
@@ -34,73 +43,56 @@
 
     // Perform Validations
     // Hint: Write these in private/validation_functions.php
-    if (is_blank($firstname)){
+    if (empty($firstname)){
       array_push($errors, "Enter your first name." );
     }elseif (!has_length($firstname, [2,255])) {
       array_push($errors, "first name should be between 2 and 255 characters.");
     }
-    if (is_blank($lastname)){
+    if (empty($lastname)){
       array_push($errors,"Enter your last name.");
     }elseif (!has_length($lastname, [2, 255])){
       array_push($errors, "last name should between 2 and 255 characters.");
     }
-    if (is_blank($email)){
+    if (empty($email)){
       array_push($errors, "Enter your email address.");
+	  echo "email is empty"; 
     }elseif (!has_length($email, [8, 255])){
       array_push($errors,"email should be between 8 and 255 characters.");
     }elseif (!has_valid_email_format($email)){
       array_push($errors, "enter valid email address.");
     }
-    if (is_blank($user)) {
+    if (empty($user)) {
       array_push($errors, "Enter a username.");
     }elseif (!has_length($user, [8,255])){
       array_push($errors, "username should be between 8 and 255 characters.");
     }
     // if there were no errors, submit data to database
-
-    if (empty($errors)){
-      // Write SQL INSERT statement
-      $firstname = mysql_real_escape_string($firstname);
-      $lastname = mysql_real_escape_string($lastname);
-      $email = mysql_real_escape_string($email);
-      $user = mysql_real_escape_string($user);
-    }
+	if (empty($errors)){
+     // Write SQL INSERT statement
+     $firstname = mysqli_real_escape_string($db, $firstname);
+     $lastname = mysqli_real_escape_string($db, $lastname);
+     $email = mysqli_real_escape_string($db, $email);
+     $user = mysqli_real_escape_string($db, $user);
+  
     $date = date('Y-m-d H:i:s');
     $sql = "INSERT INTO users(first_name, last_name, email, username, created_at) VALUES('$firstname','$lastname', '$email', '$user', '$date')";
     $result = db_query($db, $sql);
-    if(!$result) {
+    if($result) {
         db_close($db);
         redirect_to('registration_success.php');
-       } 
+      } 
     else {
         echo db_error($db);
         db_close($db);
         exit;
      }
-
+	}
+  }
  
 ?>
-
-<div id="main-content">
-  <h1>Register</h1>
-  <p>Register to become a Globitek Partner.</p>
-
-  <?php
-  if (! empty($errors)){
-    display_errors($errors);
-  }
-  ?>
-
-  <form action = "register.php" method ="post">
-    First name: <input type="text" name = "first_name" value=""/>
-    Last name: <input type="text" name="last_name" value=""/><br/>
-    Email: <input type="text" name="email" value =""/><br/>
-    Username:<input type="text" name="username" value=""/><br />
-    <br />
-    <input type="submit" name="submit" value="Submit" />
-  </form>
-
-</div>
-</body>
+	<?php if (!empty($errors)){
+		echo display_errors($errors);
+		exit; 
+	}
+	?>
 <?php include(SHARED_PATH.'/footer.php'); ?>
-</html>
